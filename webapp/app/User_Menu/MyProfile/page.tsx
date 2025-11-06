132ae7184dae04e38043a443c545067567ccdf3c
Matej Vujevic <147540118+MatejVuj@users.noreply.github.com>
2025-11-06 18:18:10 +0100

    Commiting webapp code

---
+++ webapp/app/User_Menu/MyProfile/page.tsx
@@ -0,0 +1,10 @@
+import { NavigationUserMenu } from "@/components/navigationUserMenu";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-10">
+      <NavigationUserMenu />
+      <h1 className="text-6xl font-extrabold tracking-tight">My Profile</h1>
+    </main>
+  );
+}
